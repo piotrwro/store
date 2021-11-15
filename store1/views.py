b47ec7486c1e989ea13fca658401b246fc0586b7
piotrwro <piotrwrobel76@interia.pl>
2021-11-15 16:04:37 +0100

    Changed views

--- store1/views.py
+++ store1/views.py
@@ -11,8 +11,10 @@ def product_list(request):
 
 def product(request, pk):
     item = get_object_or_404(Product, pk=pk)
-    product_image = Product.objects.filter(pk=pk).prefetch_related('images').first()
+   # product_image = Product.objects.filter(pk=pk).prefetch_related('images').first()
+    images = item.images.all()
+    print(images)
 
 
-    return render(request, 'store1/product.html', {'product': item, "image": product_image})
+    return render(request, 'store1/product.html', {'product': item, "im": images})
 
